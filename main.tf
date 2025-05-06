e48e04fc1667f52f24a23f5b07d6f83bd867c49f
Sandor Bakos <dev.sndr@bksml.com>
2025-05-06 21:44:53 +0200

    (feat) Create VPC Flow Log Module (#2)
    * Implement basic functionalities

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-#
+resource "aws_flow_log" "main" {
+  vpc_id                   = var.vpc_id
+  log_destination          = var.log_collection_bucket_arn
+  traffic_type             = var.traffic_type
+  max_aggregation_interval = var.max_log_aggregation_interval_in_seconds
+  log_destination_type     = "s3"
+  destination_options {
+    per_hour_partition         = var.log_per_hour_partition
+    file_format                = "parquet"
+    hive_compatible_partitions = true
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,29 @@
-#
+variable "log_collection_bucket_arn" {
+  description = "Log collection S3 Bucket ARN"
+  type        = string
+  nullable    = false
+}
+
+variable "vpc_id" {
+  description = "VPC ID to attach to"
+  type        = string
+  nullable    = false
+}
+
+variable "traffic_type" {
+  description = "The type of traffic to capture. Valid values: ACCEPT,REJECT,ALL."
+  type        = string
+  default     = "ALL"
+}
+
+variable "max_log_aggregation_interval_in_seconds" {
+  description = "The maximum interval of time (in seconds), during which a flow of packets is captured and aggregated into a flow log record. Valid Values: `60` (1 minute) or `600` (10 minutes)."
+  type        = number
+  default     = 600
+}
+
+variable "log_per_hour_partition" {
+  description = "Indicates whether to partition the flow log per hour, different than `max_log_aggregation_interval_in_seconds` in terms of aggregating at the destination"
+  type        = bool
+  default     = false
+}
